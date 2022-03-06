c2490a7c59491f18dbcbf6dd3164433e49aaf796
carvalhodaniel <carvalhodanielg@gmail.com>
2022-03-06 09:37:12 -0300

    Edit button feature

--- scripts.js
+++ scripts.js
@@ -20,38 +20,58 @@ addButton.addEventListener('click', (event) => {
     CreateNewDiv(newTask)
     mainTask.value = ""
     
+
+        // edit.addEventListener('click', (event)=> {
+
+        //         if(edit.value == 'Edit'){
+        //             edit.value = 'Save'
+
+        //             textInput.removeAttribute ("readonly")
+        //             textInput.focus()
+        //         }else{
+        //             edit.value = 'Edit'
+        //             textInput.setAttribute ('readonly','readonly')
+        //         }
+
+
+
+        // })
+
  
 })
+
+
+
 
 function CreateNewDiv(){
     let newTask = mainTask.value
     //console.log(newTask)
 
-    var tasks = document.createElement('div')
+    tasks = document.createElement('div')
     tasks.classList.add('tasks')
 
     containerTasks.appendChild(tasks)
 
-    var checkbox = document.createElement('input')
+    checkbox = document.createElement('input')
     checkbox.type = 'checkbox' 
     checkbox.name = 'checkbox'
     checkbox.id = 'checkbox'
     checkbox.value = 'checkbox'
 
-    var textInput = document.createElement('input')
+    textInput = document.createElement('input')
     textInput.type = 'text'
     textInput.name = 'task-description'
     textInput.id = 'task-description'
     textInput.value = newTask
     textInput.setAttribute('readonly', 'readonly')
 
-    var edit = document.createElement('input')
+    edit = document.createElement('input')
     edit.type = 'button'
     edit.name = 'edit-button'
     edit.id = 'edit-buttonn'
     edit.value = 'Edit'
 
-    var remove = document.createElement('input')
+    remove = document.createElement('input')
     remove.type = 'button'
     remove.name = 'remove-button'
     remove.id = 'remove-buttonn'
