6ee6bae49a3e0672631bd37fdcfa1cc5824f540f
carvalhodaniel <carvalhodanielg@gmail.com>
2022-03-10 14:32:30 -0300

    Removing future features and cleaning the code

--- scripts.js
+++ scripts.js
@@ -2,15 +2,16 @@ const mainTask = document.getElementById('main-task')
 const addButton = document.getElementById('add-button')
 const containerTasks = document.getElementById('container')
 const clearAll = document.getElementById('clear')
+const all = document.getElementById('all')
+const todoDiv = document.getElementById('todo-class')
+
 mainTask.focus()
 var cont = 0
 var checkCounter = 0
 
 
-// console.log(addButton)
+
 addButton.addEventListener('click', (event) => {
-   
-    
     let newTask = mainTask.value
     mainTask.focus()
 
@@ -19,52 +20,36 @@ addButton.addEventListener('click', (event) => {
     return
     }
 
-    //mainTask.value = "" //apaga o valor do input quando clica no botão
-
     CreateNewDiv(newTask);
+
         mainTask.value = "";
-    
 
         edit.addEventListener('click', (event)=> {
-              
-                    
-                    let idNumber = event.target.id.slice(11) //pega o numero da div pai do botão
-                    let buttonClicked = event.target
-                    let textSelected = document.getElementById('task-description'+idNumber)
+                                  
+            let idNumber = event.target.id.slice(11) //pega o numero da div pai do botão
+            let buttonClicked = event.target
+            let textSelected = document.getElementById('task-description'+idNumber)
                     
                 if(buttonClicked.value == 'Edit'){
                     buttonClicked.value = 'Save'
-
                     textSelected.removeAttribute ("readonly")
                     textSelected.focus()
                 }else{
                     buttonClicked.value = 'Edit'
-                    textSelected.setAttribute ('readonly', 'readonly') //teste
-                    
+                    textSelected.setAttribute ('readonly', 'readonly') //teste 
                 }  
         })
 
 
         remove.addEventListener('click', (event)=> {
             
-             let idNumberRemove = event.target.id.slice(14)
-            //console.log('id number' + idNumberRemove)
-            let divSelected = document.getElementById(idNumberRemove)
-            //containerTasks.removeChild(divSelected)           
-            divSelected.classList.add('hide')
-            
-            //Original abaixo
-            // let idNumber = event.target.id.slice(14)
-            // console.log('id number' + idNumber)
-            // let divSelected = document.getElementById(idNumber)
-            // containerTasks.removeChild(divSelected)  
-            
+            let idNumberRemove = event.target.id.slice(14)
+            let divSelected = document.getElementById(idNumberRemove)          
+            divSelected.classList.add('hide')   
         })
         
         checkbox.addEventListener('change', function(){
             
-            // toDoActivities(cont);  //if it doesn't work, REMOVE!!!
-
             for(let i=1; i++ ; i<checkCounter){
        
                 checkNumber = document.getElementById('checkbox' + (i-1))
@@ -76,24 +61,12 @@ addButton.addEventListener('click', (event) => {
                     text.classList.add('line-through')
                 }else if(checkNumber.checked==false){
                     var text = document.getElementById('task-description'+(i-1)) 
-                    text.classList.remove('line-through') 
-
-                   
+                    text.classList.remove('line-through')    
                 }
                 
             }
-
-            
-            
-
-   
         })
-
 })
-
-
-    
-
 
 function CreateNewDiv(){
     let newTask = mainTask.value
@@ -141,8 +114,6 @@ function CreateNewDiv(){
     tasks.appendChild(textInput)
     tasks.appendChild(edit)
     tasks.appendChild(remove)
-
-
 }
 
 
@@ -160,22 +131,3 @@ clearAll.addEventListener('click', (event)=> {
     }
 
 })
-
-
-
-// function toDoActivities(){
-//     console.log('arrop')
-//     let tasksHide = 1
-//     for (let activities = 1; activities++; activities<checkCounter){
-        
-
-//         divCount = document.getElementById(activities)
-
-//         if (divCount.classList.contains("hide")){
-//             tasksHide ++
-//         }
-
-
-
-//     }
-// }
